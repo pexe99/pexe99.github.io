4ea34c4d68f9632cb1cb497f3c1e345fb5ddf957
DongRyeong Lee <dr11291129@gmail.com>
2024-09-02 01:04:18 +0900

    chore: change mobile meta tag
    - 더는 사용되지 않는 meta tag를 수정했습니다.

--- src/components/seo.js
+++ src/components/seo.js
@@ -46,7 +46,7 @@ function Seo({ description, title, children }) {
         name="viewport"
         content="width=device-width, initial-scale=1, viewport-fit=cover"
       ></meta>
-      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
       {children}
     </>
   )
