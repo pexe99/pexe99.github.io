27e76e03a6fdf7910f1126fa1f68f751caa73be3
DongRyeong Lee <dr11291129@gmail.com>
2024-09-03 00:14:46 +0900

    chore: edit Toc import code
    - 배포 과정에서 오류 수정을 위해 Toc import 코드를 일부 수정했습니다.

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Toc from "../components/Toc"
 import MainTitle from "../components/MainTitle"
 import MarkdownWrapper from "../components/MarkdownWrapper"
 import PostLinks from "../components/PostLinks"
+import Toc from "../components/Toc"
 
 const PostTemplate = ({ data }) => {
   const post = data.markdownRemark
