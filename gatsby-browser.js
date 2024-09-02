bd934b5aca3949e01bb7405545afceef3581966a
DongRyeong Lee <dr11291129@gmail.com>
2024-09-02 17:51:16 +0900

    feat: add scroll initial position
    - 페이지 초기 로드 시 최상단으로 위치가 고정되도록 코드를 추가했습니다.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,8 @@
 import CustomLayout from "./wrapPageElement"
 require("katex/dist/katex.min.css")
+
 export const wrapPageElement = CustomLayout
+
+export const onInitialClientRender = () => {
+  window.scrollTo(0, 0)
+}
