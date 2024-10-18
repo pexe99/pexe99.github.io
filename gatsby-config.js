fe6c3e3fbb4ef028004b5cf1abfdc6e89aa4db17
DongRyeong Lee <dr11291129@gmail.com>
2024-10-18 15:51:17 +0900

    feat: install gatsby-plugin-robots-txt
    - robots.txt 파일 생성을 위한 플러그인을 추가했습니다.

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,12 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
